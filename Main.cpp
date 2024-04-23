acf74de4e9cc719e8ea61d228b52014853a8f51a
Mehar <117765054+Meharvir@users.noreply.github.com>
2024-04-22 18:27:22 -0700

    Update Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,32 @@
+#include "Edge.h"
+#include <iostream>
+
+int main(){
+
+    // start of the initial pipeline that loads the test case file stream from std::cin
+
+    int numOfVertices;
+    int numOfEdges;
+
+    if(!std::cin.eof()){
+        std::cin >> numOfVertices;
+        std::cin >> numOfEdges;
+    }
+    else{
+        std::cout<<"Input not found!"<<std::endl;
+        return NULL;
+    }
+
+    while(!std::cin.eof()){
+        int startVertice;
+        int endVertice;
+        std::cin >> startVertice;
+        std::cin >> endVertice;
+        Edge* newEdge = new Edge(startVertice, endVertice);
+        // Here is where you load up the Graph object
+    }
+
+    // And here is where you start working on the three tasks
+
+    return 0;
+}
